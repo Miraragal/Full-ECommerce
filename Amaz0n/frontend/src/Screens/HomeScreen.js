63ea6bf37a7e32d468411c15037a8bbd17f8ccf3
Miriam <miraragal@gmail.com>
2020-09-12 10:51:39 -0600

    Fetch Server Data

--- Amaz0n/frontend/src/Screens/HomeScreen.js
+++ Amaz0n/frontend/src/Screens/HomeScreen.js
@@ -1,13 +1,31 @@
-import React from "react";
-import data from "../data";
+import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
+import axios from 'axios';
 
 function HomeScreen(props) {
+  
+//vamos a definir a hook const [state, setstate] = useState(initialState)
+const [products, setProduct] = useState([]);
+ 
+// fetch data from frontserver
+ useEffect(() => {
+   // instalamos axios para
+   const fetchData= async ()=> {
+     const {data}= await axios.get("/api/products");
+     setProduct(data)
+   }  
+   fetchData(); // we need to call fetchData 
+   return () => {
+    //
+   }
+ }, []) // empty array means that this line of code (effect) only run a component Dimount
+
   return (
     //Movemos la ul de App.js a HomeScreen
       <ul className="products">
-        {data.products.map(product => // Combertimos lista productos en un array. Para ello, tenemos que crear un file 'data.js' con toda la info
-          <li key={product._id}>
+        {/* {data.products.map(product => Combertimos lista productos en un array. Para ello, tenemos que crear un file 'data.js' con toda la info */}
+        {products.map(product => // Lo sustituimos products que hemos definido en el hooks
+         <li key={product._id}> 
             <div className="product">
               {/* Introducimos link a la pag del producto en la imagen */}
               <Link to={'/product/' + product._id}>
